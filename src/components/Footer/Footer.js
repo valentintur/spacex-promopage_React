8fc38b86d9f794f21aff2580924b2865a2bc6eec
Valentin Tur <valentin.tur.it@gmail.com>
2020-10-14 18:27:57 +0200

    init project

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Logo from '../../logo.svg';
+import './footer.css';
+
+const Footer = () => (
+
+    <footer class="footer">
+		<img src={Logo} alt="logo Space X" class="logo"/>
+		<nav class="footer-nav">
+			<ul class="list">
+				<li class="item"><a href="#" rel="noopener noreferrer" target='_blank' class="item-link">Elon Musk Twitter</a></li>
+				<li class="item"><a href="#" rel="noopener noreferrer" target='_blank' class="item-link">Twitter</a></li>
+				<li class="item"><a href="#" rel="noopener noreferrer" target='_blank' class="item-link">Flickr</a></li>
+				<li class="item"><a href="#" rel="noopener noreferrer" target='_blank' class="item-link">Website</a></li>
+			</ul>
+		</nav>
+		<p class="footer-text">
+			For additional questions, contact
+			<a class="footer-link" href="mailto:rideshare@spacex.com"
+			>rideshare@spacex.com</a
+			>
+		</p>
+
+	</footer>
+
+)
+
+export default Footer;
